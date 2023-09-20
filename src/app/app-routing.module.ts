fb1540840be9f7577ad63b4d94c8b691f146f0c0
Kelvin Soares <kelvin.fe.soares@gmail.com>
2023-09-19 22:32:40 -0300

    Project setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pokelist/components/home/home.component';
+import { PokemonDetailsComponent } from './pokelist/components/pokemon-details/pokemon-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    title: 'Home',
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    title: 'Pokemon Details',
+    path: 'details/:id',
+    component: PokemonDetailsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokelistModule } from './pokelist/pokelist.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, PokelistModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
